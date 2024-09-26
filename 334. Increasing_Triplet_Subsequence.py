8d03656df57f77a78da82de3621420059a45a882
mahala2 <abhijit.mahal2@mail.dcu.ie>
2024-09-26 12:16:45 +0100

    334

---
+++ 334. Increasing_Triplet_Subsequence.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def increasingTriplet(self, nums):
+        if len(nums) < 3:
+            return False
+
+        first = float('inf')
+        second = float('inf')
+
+        for n in nums:
+            if n <= first:
+                first = n
+            elif n <= second:
+                second = n
+            else:
+                return True
+
+        return False
+        
